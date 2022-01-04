65d6c36285c37b50211bc300764091e449b8052e
GANESHCHILLZ <95964859+GANESHCHILLZ@users.noreply.github.com>
2022-01-04 18:33:48 +0530

    Add files via upload

---
+++ answers.js
@@ -0,0 +1,19 @@
+/*1) Java Script
+==>js is a simple programming lauguage that runs in any browser JavaScript Engine.
+====> javascript is a high leve; programming language that uses the concept of Oops but it is based on prototype inheritance.
+Nodejs
+===> Nodejs provies a ver vast frame work and includes many libaries to support .
+====>Nodejs is a cross platform and opensource  js runtime enviroment that allows the javascript to run on the server side. 
+
+
+a)typeof(1) ===> Number
+b)typeof(1.1)===> Number
+c)typeof('1.1')===> String
+e)typeof(true)======> Boolean
+f)typeof(null)========>object
+g)typeof(undefined)======>undefined
+h)typeof([]) ===============>object
+i)typeof({})==================> object
+j)typeof(NaN)======================> number
+*/
+
